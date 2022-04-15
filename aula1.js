3ff8f9069c9b5461599dee11047d2a088eb2ed27
Juliana Domingues <julianasdomingues02@gmail.com>
2022-04-15 17:28:19 -0300

    Primeira aula

---
+++ aula1.js
@@ -0,0 +1,14 @@
+var programacao = "Javascript e Python";
+console.log(programacao.replace("JavaScript", "Java"));
+
+// determina tamanho da string
+
+console.log(programacao.length);
+
+//executa a letra da posição determinada
+
+console.log(programacao.charAt(4));
+
+// procura uma determinada string e retorna a 1º posição 
+
+console.log(programacao.indexOf("Python"));
